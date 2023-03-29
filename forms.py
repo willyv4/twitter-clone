80424f4ad9b31a9e77be5f99b82da9dc631a7452
William Valadez <billy.valadez@icloud.com>
2023-03-29 00:37:20 -0400

    added profile route, fixed bugs, set up env

---
+++ forms.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField, HiddenField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class MessageForm(FlaskForm):
+    """Form for adding/editing messages."""
+
+    text = TextAreaField('text', validators=[DataRequired()])
+
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+
+class UpdateUserForm(FlaskForm):
+    """ update user form """
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('Image URL')
+    header_image_url = StringField('Background Image URL')
+    bio = StringField("Bio")
